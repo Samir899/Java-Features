0902c5bfaeb1b0c99bf42bd98d33d94aa6f323f9
Samir899 <sameerkmr823@gmail.com>
2025-07-05 14:10:08 +0530

    Method Reference - Consumer

--- java-8/src/methodReference/ConsumerMethodReferenceExample.java
+++ java-8/src/methodReference/ConsumerMethodReferenceExample.java
@@ -1,4 +1,23 @@
 package methodReference;
 
+import data.StudentData;
+import entity.Student;
+import java.util.function.Consumer;
+
 public class ConsumerMethodReferenceExample {
+
+    /**
+     * className::methodName
+     */
+    static Consumer<Student> c1 = System.out::println;
+
+    /**
+     * className::instanceMethodName
+     */
+    static Consumer<Student> c2 = Student::printActivities;
+
+    public static void main(String[] args) {
+        StudentData.getAllStudent().forEach(c1);
+        StudentData.getAllStudent().forEach(c2);
+    }
 }
