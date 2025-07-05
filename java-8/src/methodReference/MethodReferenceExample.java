9e2488598cd560430fa40161a49d8fef0125691c
Samir899 <sameerkmr823@gmail.com>
2025-07-05 14:37:07 +0530

    Method reference refactor

--- java-8/src/methodReference/MethodReferenceExample.java
+++ java-8/src/methodReference/MethodReferenceExample.java
@@ -1,4 +1,19 @@
 package methodReference;
 
+import data.StudentData;
+import entity.Student;
+
+import java.util.function.Predicate;
+
 public class MethodReferenceExample {
+
+    static Predicate<Student> p1 = MethodReferenceExample::greaterThanGradeLevel;
+
+    public static boolean greaterThanGradeLevel(Student student){
+        return student.getGradeLevel()>=3;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(p1.test(StudentData.studentSupplier.get()));
+    }
 }
