8b5ab4cf28bf4c8f3336a45dbef410720cf0a024
Samir899 <sameerkmr823@gmail.com>
2025-07-08 11:58:16 +0530

    Constructor Reference

--- java-8/src/constructorReference/ConstructorReferenceExample.java
+++ java-8/src/constructorReference/ConstructorReferenceExample.java
@@ -1,4 +1,20 @@
 package constructorReference;
 
+import entity.Student;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class ConstructorReferenceExample {
+
+    static Supplier<Student> studentSupplier = Student::new;
+    static Function<String, Student> studentFunction = Student::new;
+    static BiFunction<String, Integer, Student> studentBiFunction = Student::new;
+
+    public static void main(String[] args) {
+        System.out.println("Empty Student: " + studentSupplier.get());
+        System.out.println("Student with Name: " + studentFunction.apply("Samir"));
+        System.out.println("Student with Name and Grade: " + studentBiFunction.apply("Samir", 5));
+    }
 }
